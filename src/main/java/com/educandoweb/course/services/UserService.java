d7e254a6278e595ba0306597c9c8828f0d0a2b51
Fernando Okamura <fokamura10@gmail.com>
2024-02-07 23:09:22 -0300

    User update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -28,4 +28,14 @@ public User insert (User obj) {
 public void delete (Long id) {
 	repository.deleteById(id);
 }
+public User update(Long id, User obj) {
+	User entity = repository.getReferenceById(id);
+	updateData(entity, obj);
+	return repository.save(entity);
+}
+private void updateData(User entity, User obj) {
+	entity.setName(obj.getName());
+	entity.setEmail(obj.getEmail());
+	entity.setPhone(obj.getPhone());
+}
 }
